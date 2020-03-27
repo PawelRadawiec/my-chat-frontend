8089d17b6beec655e4f0958a54bb97752e37ee8d
pawelr <pawelr@lsnova.pl>
2020-03-27 13:45:21 +0100

    basic chat message reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ChatMessage} from './model/chat-message.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-chat-frontend';
+export class AppComponent implements OnInit {
+  messageForm: FormGroup;
+  userForm: FormGroup;
+  messages: ChatMessage[] = [];
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  ngOnInit() {
+    this.initForms();
+  }
+
+  initForms() {
+    this.messageForm = this.formBuilder.group({
+      message: [Validators.required]
+    });
+    this.userForm = this.formBuilder.group(({
+      username: [Validators.required]
+    }));
+  }
+
+
 }
---
+++ src/app/model/chat-message.model.ts
@@ -0,0 +1,7 @@
+
+
+export class ChatMessage {
+  message: string;
+  from: string;
+  to: string;
+}
